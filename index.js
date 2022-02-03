71b428b36968c6fad9ed6d4cddef3666cf2e509a
patelsaheb1219 <kaumik1219@gmail.com>
2022-02-03 15:59:16 -0500

    Setup react app in node.js

--- index.js
+++ index.js
@@ -61,6 +61,16 @@ app.post('/checkout', async (req, res, next) => {
   res.json({ error, status });
 })
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Port is running on ${PORT}`);
 })
